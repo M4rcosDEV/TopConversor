d490d09b0b5880ebafbd7b75265d39dea9f1092b
M4rcosDEV <marcosifba01@gmail.com>
2025-01-05 19:03:55 -0300

    `Update ColunasInformacao component with new product and client data`

--- src/components/ColunasInformacao/ColunasInformacao.jsx
+++ src/components/ColunasInformacao/ColunasInformacao.jsx
@@ -20,12 +20,66 @@ export default function ColunasInformacao() {
     const {selectedTypeOption} = useContext(DataContext);
       const [colunaSelected, setColunaSelected] = useState([]);
     
-      const produto =[
-        'numprod', 'descprod', 'preco','numgrad','descgrad','tam','numesto','qtd','local'
+      const produto = [
+         //tab_prod
+        'codprod', 'nomprod', 'espprod', 'graprod', 'codfabr', 'codfili', 'cl1prod', 'cl2prod', 
+        'cl3prod', 'cl4prod', 'depprod', 'tipprod', 'obsprod', 'desresu', 'fgedeci', 'xyzprod', 
+        'atuprod', 'cod_ipi', 'cod_irr', 'subfede', 'ncmprod', 'imgprod', 'refgrad', 'codgrad', 
+        'codregr', 'filfabr', 'marprod', 'negprod', 'indprod', 'tipipro', 'genepro', 'lisipro', 
+        'ex_prod', 'indprop', 'codprop', 'filprop', 'endprop', 'ctnprod', 'cbccpro', 'recprod', 
+        'prociap', 'prodepr', 'regr_sa', 'regr_en', 'parfide', 'escombo', 'agrgrad', 'lismate', 
+        'ctnfatu', 'syngrad', 'prouuid', 'negunid', 'divisa1', 'divisa2', 'divisa3', 'divisa4', 
+        'divisa5',
+
+         //tab_grad
+        'codgrad', 'codprod', 'codidiv', 'codisub', 'unvgrad', 'uncgrad', 'estmini', 'pesliqu', 
+        'pesbrut', 'desmaxi', 'percomi', 'margrad', 'fatconv', 'obsgrad', 'lo1grad', 'lo2grad', 
+        'ptopedi', 'fgecomi', 'refgrad', 'gragrad', 'filprod', 'lucgrad', 'pzorese', 'fgenser', 
+        'fgelote', 'fgelotv', 'cfdgrad', 'cffgrad', 'ncmgrad', 'dtagrad', 'ctngrad', 'md5grad', 
+        'codante', 'metgrad', 'tipcomi', 'sercomp', 'diaprox', 'mkpgrad', 'exc_ncm', 'codcest', 
+        'bcpicms', 'bcpicst', 'usesite', 'gtingra', 'proanpc', 'proanpd', 'tmpgrad', 'paigrad', 
+        'itempai', 'embgrad', 'maxacre', 'pglpgra', 'pgnngra', 'estaten', 'vpartgr', 'ml_grad', 
+        'diasgra', 'tribuid', 'iterele', 'itecnpj', 'datvenc', 'diavenc', 'livreid', 'cobenef', 
+        'usegrad', 'purgrad', 'gergrad', 'metrage', 'estomax', 'tzgrad', 'grauuid', 'ml_cate', 
+        'ml_idpd', 'lo3grad', 'vitrine', 'cfgbala', 'codanvi', 'motanvi', 'preanvi', 'multemp', 
+        'biograd', 'impcomb', 'ufocomb', 'pufcomb', 'deslivr', 'idivi_1', 'idivi_2', 'idivi_3', 
+        'idivi_4', 'idivi_5',
+
+         //tab_esto
+        'codigra', 'estiest', 'resiest', 'filesto', 'precomp', 'ultcust', 'cusmedi', 'cuspond', 
+        'prevend', 'preprom', 'preatac', 'proplan', 'prodtin', 'prodtfi', 'curvabc', 'estnega', 
+        'forlinh', 'codesto', 'ajuprec', 'qtdatac', 'custefe', 'pr1esto', 'pr2esto', 'qtdpend', 
+        'estuuid', 'intrasi', 'chegaem', 'vlrcomp', 'qtdprom', 'vqtprom', 'lo1esto', 'lo2esto', 
+        'lo3esto', 'obsprom', 'qtdata2', 'preata2', 'giroest'
+
       ];
       
+      
       const cliente =[
-        'nomeCliente', 'IdadeCliente', 'MatriculaCliente','textrsfx'
+        //tab_parc
+        'codparc', 'nomparc', 'codfili', 'sobparc', 'cnpparc', 'regparc', 'nasparc', 'cadparc', 
+        'altparc', 'obsparc', 'homparc', 'emaparc', 'venpref', 'plapref', 'cobpref', 'spccons', 
+        'negparc', 'ultcont', 'ultvend', 'filaval', 'codaval', 'limcred', 'doctipo', 'estcivi', 
+        'sexparc', 'pesparc', 'codocup', 'nacparc', 'natparc', 'ctpparc', 'cnhparc', 'cat_cnh', 
+        'supparc', 'admparc', 'demparc', 'nomempr', 'endempr', 'comempr', 'dddempr', 'fonempr', 
+        'antempr', 'ctpseri', 'fgeparc', 'filconj', 'codconj', 'nomconj', 'cnpconj', 'nasconj', 
+        'emaconj', 'ddfconj', 'fonconj', 'ddcconj', 'celconj', 'porempr', 'tipempr', 'iprparc', 
+        'ultende', 'renparc', 'imgparc', 'dis_iss', 'motbloq', 'rntparc', 'tacparc', 'tpaparc', 
+        'ucbparc', 'datconj','atvagend', 'codrfid', 'cnhvali', 'obsconj', 'undparc', 'co_fina', 
+        'obsfina', 'agenseg', 'agenter', 'agenqua', 'agenqui', 'agensex', 'agensab', 'agendom', 
+        'usuparc', 'regprec', 'codante', 'datbloq', 'indbloq', 'senparc', 'pisparc', 'codhora', 
+        'filante', 'paiparc', 'maeparc', 'rotparc', 'falparc', 'comfale', 'oriparc', 'coddeli', 
+      'update_at', 'ctaclie', 'ctaforn', 'custpad', 'logcadw', 'fgelgpd', 'percomi', 'dia_lib',
+
+        //tab_ende
+        'codparc', 'fonende', 'cgcende', 'ieende', 'endende', 'baiende', 'codcida', 'cepende', 
+        'numende', 'nomende', 'filparc', 'faxende', 'celende', 'ddd_fon', 'complem', 'referen', 
+        'tiplogr', 'nommuni', 'uf_ende', 'paiende', 'ordende', 'iesende', 'sufende', 'optsimp', 
+        'im_ende', 'ctbicms', 'ramtelf', 'nomgest', 'cpfgest', 'emagest', 'crggest', 'ftaparc', 
+        'prlparc', 'cpsparc', 'dafparc', 'temresi', 'numfunc', 'vlraluq', 'cnaeend', 'tpeende', 
+        'ip_ende', 'pe_ende', 'ireende', 'gpslong', 'gpslati', 'asfende', 'estende', 'entende', 
+        'seqende', 'nom_end', 'iiedest', 'regimet', 'coddeli', 'codtaen', 'update_at', 'fgeende', 
+        'idendpw', 'azulzon'
       ];
       
       useEffect(() => {
